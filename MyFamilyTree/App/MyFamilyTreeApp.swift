791871db25a450b68d24cb42714e284951fc7f12
aries-rst <68280428+aries-rst@users.noreply.github.com>
2026-09-26 12:12:57 +0500

    Add StoreManager to MyFamilyTreeApp

--- MyFamilyTree/App/MyFamilyTreeApp.swift
+++ MyFamilyTree/App/MyFamilyTreeApp.swift
@@ -3,12 +3,17 @@ import SwiftUI
 @main
 struct MyFamilyTreeApp: App {
     @StateObject private var appState = AppState()
+    @StateObject private var store = StoreManager()
 
     var body: some Scene {
         WindowGroup {
             RootTabView()
                 .environmentObject(appState)
+                .environmentObject(store)
                 .tint(Theme.wine)
+                .task {
+                    store.start(appState: appState)
+                }
         }
     }
 }
